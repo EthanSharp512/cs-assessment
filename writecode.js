eab47527c0eb58aad7cb2248780a7404b7f05955
Ethan Sharp <ethansharpdev@gmail.com>
2022-05-27 10:29:32 -0600

    is pangram and longest word

--- writecode.js
+++ writecode.js
@@ -23,3 +23,36 @@ function hasUniqueChars(word) {
   }
   
   //The runtime for the hasUniqueChars function would be O(n)
+
+
+
+const isPangram = (str) => {
+   str = str.toLowerCase();
+   const { length } = str;
+   const alphabets = 'abcdefghijklmnopqrstuvwxyz';
+   const alphaArr = alphabets.split('');
+   for(let i = 0; i < length; i++){
+      const el = str[i];
+      const index = alphaArr.indexOf(el);
+      if(index !== -1){
+         alphaArr.splice(index, 1);
+      };
+   };
+   return !alphaArr.length;
+};
+
+//The runtime for the isPangram function would be O(n)
+
+
+
+function longestWord(arr) {
+    let word = "";
+    for (let i = 0; i < arr.length; i++) {
+      if (word.length < arr[i].length) {
+        word = arr[i];
+      }
+    }
+    return word;
+  }
+
+//The runtime of longestWord is O(n)
